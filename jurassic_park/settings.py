6c286f61ad0d322ca4c7e39c219e299b7a05f60b
zorrillalol <luis56587@gmail.com>
2020-02-12 08:22:14 -0600

    Selecciona sqlite como DBMS y agrega configuración de travis

--- jurassic_park/settings.py
+++ jurassic_park/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'jurassic_park.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jurassic_park',
-        'USER' : 'dinouser',
-        'PASSWORD' : 'dino123@',
-        'HOST': '127.0.0.1',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
